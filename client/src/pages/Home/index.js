ca11b11b90d42f1b7da856b2c7e57d6797ad3793
Edita Pesa <editapesa@gmail.com>
2021-06-26 10:25:25 -0400

    important cloudinary fix, updated libraries, fix concurrently

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,2 +1,19 @@
 //this is for the first screen in wireframe example
 //displays "what are you waiting for?" & start button
+
+import React from "react";
+import Header from '../../components/Header';
+
+function Home() {
+    return (       
+        <div>
+            <Header />
+            <h1>WHAT ARE YOU WAITING FOR?</h1>
+            <p>Show the world what you want most out of life.</p>
+            <p>People who set goals are 7x more likely to reach them.</p>
+            <button>START RIGHT NOW</button>
+        </div>
+    );
+}
+
+export default Home;
--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,2 +1,73 @@
 //this is for the 3rd wire frame screen 
 //displays user option to sign up
+
+import React, { useState } from 'react';
+import Container from '../../components/Container';
+import Col from '../../components/Col';
+import Header from '../../components/Header';
+import Row from '../../components/Row';
+
+const Signup = () => {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log('username is ' + username);
+        console.log('email is ' + email);
+        console.log('password is ' + password);
+    };
+
+    return (
+        <div>
+            <Header />
+            <div className='mt-4'>
+                <h2>Sign Up</h2>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <Container className='mt-3 px-5'>
+                    <Row className='form-group'>
+                        <Col size='12'>
+                            <input
+                                className='form-control'
+                                type='text'
+                                placeholder='username'
+                                name='username'
+                                onChange={e => setUsername(e.target.value)}
+                            />
+                        </Col>
+                    </Row>
+                    <Row className='form-group'>
+                        <Col size='12'>
+                            <input
+                                className='form-control'
+                                type='email'
+                                placeholder='email address'
+                                name='email'
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </Col>
+                    </Row>
+                    <Row className='form-group'>
+                        <Col size='12'>
+                            <input
+                                className='form-control'
+                                type='password'
+                                placeholder='password'
+                                name='password'
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </Col>
+                    </Row>
+                    <button className='btn btn-success' type='submit'>
+                        Submit
+                    </button>
+                </Container>
+            </form>
+        </div>
+    );
+}
+
+export default Signup;
+
