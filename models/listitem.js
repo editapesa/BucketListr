d71d1818e01d30cc4a5cd779a7e9e3c634d862ba
SHuo Zhu <shuozhu@gmail.com>
2021-07-08 17:57:29 -0400

    some research

--- models/listitem.js
+++ models/listitem.js
@@ -10,4 +10,15 @@ const listitemSchema = new Schema({
 
 const ListItem = mongoose.model('ListItem', listitemSchema);
 
+const listitemSchema = new Schema({
+    title: { type: String, required: true },
+    url:  { type: String },
+    image: { type: String },
+    date: { type: Date, default: Date.now }
+});
+
+const TaskList = mongoose.model('taskList', taskListSchema)
+
+const listColumn = 
+
 module.exports = ListItem;
