5e854e20ade10eb19c9000acbc4499a492f1eabc
Edita Pesa <editapesa@gmail.com>
2021-07-07 23:36:35 -0400

    get BucketList & Profile pgsto display. Add links to Dashboard. Delete files not used in utils.

--- client/src/pages/BucketList.js
+++ client/src/pages/BucketList.js
@@ -1,61 +1,53 @@
 //this is for the 6th wire frame screen
 //displays polished bucket list with user name, pic, bio/overview
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_LISTITEMS, LOADING } from '../utils/actions';
+import { List, ListItem } from '../components/List';
 import API from '../utils/API';
-import Col from '../components/Col';
+//import Col from '../components/Col';
 import Row from '../components/Row';
 import Container from "../components/Container";
 
 function BucketList() {
-    const [state, dispatch] = useStoreContext();
+    const [listItems, setListItems] = useState([])
   
-    const getListItems = () => {
-        dispatch({ type: LOADING });
+    useEffect(() => {
+      loadListItems()
+    }, [])
+
+    function loadListItems() {
         API.getListItems()
-          .then(results => {
-            dispatch({
-              type: UPDATE_LISTITEMS,
-              listItems: results.data
-            });
-          })
+          .then(res =>
+              setListItems(res.data)
+          )
           .catch(err => console.log(err));
-      };
-    
-      useEffect(() => {
-        getListItems();
-      });
+    };
 
       // figure out how to get user info
-      let user;
 
       return (
-          <div>
-              <image>{user.photo}</image>
+          <Container>
+              {/* <image>{user.photo}</image>
               <h1>{user.username}</h1>
-              <p>{user.bio}</p>
+              <p>{user.bio}</p> */}
               {/* change to display only 8 list items  */}
-              {state.listItems.length ? (
-                <Container>
-                {state.listItems.map(listItem => (
-                    <Row>
-                    <Col> {listItem._id}
-                    <Link to={'/listitem/' + listItem._id}>
-                        {listItem.title}
-                    </Link>
-                    </Col>
-                    <Col> {listItem.image}
-                    </Col>
-                    </Row>
-                ))}
-              </Container>
-              ) : (
-                <h3>You haven't added any list items yet!</h3>
-              )}
-          </div>
+              <Row>
+                {listItems.length ? (
+                    <List>
+                        {listItems.map(listItem => (
+                            <ListItem key={listItem._id}>
+                                <Link to={listItem.url + listItem._id}>
+                                    {listItem.title} 
+                                </Link>
+                            </ListItem>
+                    ))}
+                    </List>
+                  ) : (
+                    <h3>No List Items to Display</h3>
+                )}
+              </Row>
+          </Container>
       );
     }
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,6 +16,15 @@ let API =  {
     saveListItem: function(listItemData) {
         console.log(listItemData);
         return axios.post('/api/listitem', listItemData);
+    },
+
+    getUser: function(id) {
+        return axios.get('/api/user/' + id);
+    },
+
+    saveUser: function(userData) {
+        console.log(userData);
+        return axios.post('/api/user', userData);
     }
 };
 
